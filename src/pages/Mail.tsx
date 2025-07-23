3e89607f7dae4758f0d7bfdb1b72fa0846473587
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-07-23 16:42:37 +0200

    iframes for html mail

--- src/pages/Mail.tsx
+++ src/pages/Mail.tsx
@@ -1,16 +1,17 @@
 import { useQueryClient } from "@tanstack/react-query"
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import type { ParsedMail } from "../types/ParsedMail.type"
 
 const Mail = () => {
     const navigate = useNavigate()
     const params = useParams()
     const queryClient = useQueryClient()
 
-    const mails = queryClient.getQueryData(["emails-page"]) as any[] | undefined
+    const mails = queryClient.getQueryData(["emails-page"]) as ParsedMail[] | undefined
     const id = Number(params.id)
 
-    const [mail, setMail] = useState<any | null>(null)
+    const [mail, setMail] = useState<ParsedMail | null>(null)
 
     useEffect(() => {
         if (!mails || isNaN(id) || id < 0 || id >= mails.length) {
@@ -20,9 +21,23 @@ const Mail = () => {
         setMail(mails[id])
     }, [mails, id, navigate])
 
-    if (!mail) return null
 
-    return <div className="mailContainer">{mail.subject}</div>
+
+    if (!mail) return null
+    console.log(mail.html)
+    return <div className="mailContainer">
+        {mail.subject}
+
+        <iframe
+            srcDoc={mail.html}
+            style={{ width: '100%', height: '600px', border: 'none' }}
+        />
+
+
+
+
+
+    </div>
 }
 
 export default Mail
