8fdb1a2f997f1c03a381d197c835f6c8c4a3ce25
Mati <90572281+matiEU07@users.noreply.github.com>
2026-01-07 11:59:31 +0100

    added dropdown for mail selection

--- src/partials/navbar.tsx
+++ src/partials/navbar.tsx
@@ -14,12 +14,22 @@ function Nav() {
   });
   
 
+  const [isMailDropdownOpen, setIsMailDropdownOpen] = useState<boolean>(false);
 
   return (
     <nav> 
-      <div className="mailDropdown">
+      <div className="mailDropdown" onMouseDown={() => setIsMailDropdownOpen(prev => !prev)} onMouseUp={() => console.log("unclicked")}>
         mati@ep09.net
+
+          <div className={`mailDropdownContainerList ${isMailDropdownOpen ? 'show' : ''}`}>
+            
+            <div onMouseUp={() => setIsMailDropdownOpen(false)}>eszutmail@tenco.waw.pl</div>
+            
+            <div onMouseUp={() => setIsMailDropdownOpen(false)}>matiew58@gmail.com</div>
+            <div onMouseUp={() => setIsMailDropdownOpen(false)}>+ dodaj skrzynkę</div>
+          </div>
       </div>
+
       <div className="search">
         <input id="search" placeholder="przeszukaj pocztę..."/>
         <button className="searchIcon"></button>
